f4e2d2ced7403cb40138cdf21a8a961f93f860a9
Okutansoy <128052061+Okutansoy@users.noreply.github.com>
2023-03-16 14:42:36 +0100

    Add files via upload

---
+++ Variabel asicode.py
@@ -0,0 +1,11 @@
+print('Hallo was geht!')              #Hier gebe ich einein Text aus
+ganzeZahl = 34                         #Erzeugen von einer Variable vom Typ integer (ganze Zahl)
+print('Hier ist unsere ganze Zahl: ',ganzeZahl)  #Ausgabe der ganzen Zahl
+Kommazahl = 12.23                      #Erzeugen von einer Variable vom Typ (Kommazahl oder Gleitpunktzahl)
+print('Hier ist unsere Kommazahl: ',Kommazahl)    #Ausgabe der Kommazahl
+Bool = True                            #Erzeugen von einer Variable vom Typ Bool (Binär, True oder False!)
+print('Hier ist unser Bool: ',Bool)    #Ausgabe von einer Boolschen Variable
+String = 'Das hier ist ein String.'   #erzeugen eines Strings (alle Zeivhen des ASCII-CODES)
+print(String)                         #Ausgabe des Strings
+print('hier kommt eine neue Zeile \nDann gehts es hier weiter')   #neue Zeile mit Hilfe von \'')
+print('wenn wir ein Gänsefüßchen ausgeben wollen: \' oder ein Apostroph: \' ' )
